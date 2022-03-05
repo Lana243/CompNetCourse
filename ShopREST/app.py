f3a624f4e4c72b10be9906341ccf44d26d60f0f6
Lana243 <lana0771711@gmail.com>
2022-03-05 23:17:01 +0300

    add service for online shop

--- ShopREST/app.py
+++ ShopREST/app.py
@@ -1,9 +1,78 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response, request
+from flask import abort
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+items = [
+    {
+        'id': 1,
+        'name': u'Jeans',
+        'description': u'Woman, dark blue, high waist, skinny',
+    },
+    {
+        'id': 2,
+        'name': u'Sweater',
+        'description': u'Woman, woolen, Mickey Mouse',
+    }
+]
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.route('/shop/api/v1.0/items', methods=['GET'])
+def get_items():
+    return jsonify({'items': items})
+
+
+@app.route('/shop/api/v1.0/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = list(filter(lambda t: t['id'] == item_id, items))
+    if len(item) == 0:
+        abort(404)
+    return jsonify({'item': item[0]})
+
+
+@app.route('/shop/api/v1.0/items', methods=['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        abort(400)
+    item = {
+        'id': items[-1]['id'] + 1,
+        'name': request.json['name'],
+        'description': request.json.get('description', ""),
+    }
+    items.append(item)
+    return jsonify({'item': item}), 201
+
+
+@app.route('/shop/api/v1.0/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    item = list(filter(lambda t: t['id'] == item_id, items))
+    if len(item) == 0:
+        abort(404)
+    items.remove(item[0])
+    return jsonify({'result': True})
+
+
+@app.route('/shop/api/v1.0/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    item = list(filter(lambda t: t['id'] == item_id, items))
+    if len(item) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    item[0]['name'] = request.json.get('name', item[0]['name'])
+    item[0]['description'] = request.json.get('description', item[0]['description'])
+    return jsonify({'item': item[0]})
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
