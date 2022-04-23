b5609c7001fb9cb4e45bbfcc0c935e4ec7805fb3
Lana243 <lana0771711@gmail.com>
2022-04-24 00:36:38 +0300

    task ip addr done

---
+++ ip_addr/main.py
@@ -0,0 +1,11 @@
+from netifaces import interfaces, ifaddresses, AF_INET
+
+
+def print_ip4_addresses():
+    for interface in interfaces():
+        for link in ifaddresses(interface)[AF_INET]:
+            print(f'addr: {link["addr"]}, netmask: {link["netmask"]}')
+
+
+if __name__ == '__main__':
+    print_ip4_addresses()
